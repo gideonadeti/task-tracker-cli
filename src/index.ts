fed0f6cc2d1cd075235ba03b4102ee21c5dfa1ca
gideonadeti <gideonadeti0@gmail.com>
2024-09-06 13:27:20 +0000

    Define helper functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import * as path from "path";
+import * as fs from "fs";
 
 interface Task {
   id: number;
@@ -11,3 +12,22 @@ interface Task {
 }
 
 const filePath = path.join(__dirname, "tasks.json");
+
+// Helper to load tasks from the JSON file
+function loadTasks(): Task[] {
+  if (!fs.existsSync(filePath)) {
+    return [];
+  }
+  const data = fs.readFileSync(filePath, "utf-8");
+  return JSON.parse(data);
+}
+
+// Helper to save tasks to the JSON file
+function saveTasks(tasks: Task[]): void {
+  fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+}
+
+// Helper to generate a new unique task ID
+function getNextTaskId(tasks: Task[]): number {
+  return tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+}
